e147dc1a090935b97140ce019c5cebe8307f53d0
danielegalbusera <daniele.galbu@gmail.com>
2022-01-24 12:51:51 +0100

    Aggiunta home

---
+++ home.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+function over1() {
+	
+	document.getElementById ("immagine1") .style.display = "none";
+	
+	document.getElementById ("immagine2") .style.display = "block";
+	
+}
+
+
+function out1() {
+	
+	document.getElementById ("immagine1") .style.display = "block";
+	
+	document.getElementById ("immagine2") .style.display = "none";
+	
+}
+
+function over2() {
+	
+	document.getElementById("immagine1") .style.display="none";
+	
+	document.getElementById("immagine3") .style.display="block";	
+	
+}
+
+function out2() {
+	
+	document.getElementById("immagine1") .style.display="block";
+	
+	document.getElementById("immagine3") .style.display="none";
+}
+
+
+function over3() {
+	
+	document.getElementById("immagine1") .style.display="none";
+	
+	document.getElementById("immagine4") .style.display="block";
+}
+
+function out3() {
+	
+	document.getElementById("immagine1") .style.display="block";
+	
+	document.getElementById("immagine4") .style.display="none";
+}
+
+
+
+
+
